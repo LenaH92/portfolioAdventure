0675d4d3ddc4696212e6788833d0349abf7352e8
Lena Cortes <lena.cortes@usal.es>
2025-07-06 18:33:30 +0200

    i18n.js created

---
+++ src/i18n.js
@@ -0,0 +1,27 @@
+// i18n.js
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+// get dialogs files 
+import es from './locales/es.json'
+import en from './locales/en.json'
+
+
+i18n.use(initReactI18next).init({
+    //setting the dialogs options to a language for selecting it
+    resources: {
+        es: {
+            translation: es,
+        },
+        en: {
+            translation: en,
+        },
+    },
+    lng: 'en',
+    fallbackLng: 'en',
+    interpolation: {
+        escapeValue: false,
+    },
+})
+
+export default i18n
