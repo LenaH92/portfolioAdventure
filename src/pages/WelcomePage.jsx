01b686078adb6a2616c124355dd66d1e4cf20afb
Lena Cortes <lena.cortes@usal.es>
2025-07-07 20:44:31 +0200

    first try with the translation of welcomescreen

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,5 +1,20 @@
+import { useTranslation } from "react-i18next";
+
 const WelcomePage = () => {
-    return (<><h1>pagoina de bienvenida</h1></>);
+
+    const { t } = useTranslation()
+
+    const description = t("welcomeScreen.descriptiveText");
+    const opt1 = t("welcomeScreen.option1");
+    const opt2 = t("welcomeScreen.option2");
+
+
+    return (<><div>
+        <p>{description}</p>
+        <div className="optionsDiv">
+            <button> {opt1}</button>
+            <button> {opt2}</button>
+        </div></div></>);
 }
 
 export default WelcomePage;
