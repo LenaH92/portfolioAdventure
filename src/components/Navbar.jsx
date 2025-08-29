d67bfd5c3abfa11554e8407ac9c269522d16caa0
Lena Cortes <lena.cortes@usal.es>
2025-08-30 01:25:46 +0200

    starting the map in navba

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,11 @@ import LanguageSwitch from "./LanguageSwitch";
 const Navbar = () => {
     return (<>
         <LanguageSwitch />
+        <div>
+            <p>MAP</p>
+            {/* will pobably need to make a component just fo the map button so language chages (?)
+            !! Need also to ceate the map language inputs in jsons :D */}
+        </div>
     </>);
 }
 
