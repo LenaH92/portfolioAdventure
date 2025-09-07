1dfb6201bc6ba8ce88de433cb6533e8103fe2643
Lena Cortes <lena.cortes@usal.es>
2025-09-07 22:20:48 +0200

    added useTrranslation in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
+import { useTranslation } from "react-i18next";
 import LanguageSwitch from "./LanguageSwitch";
 
 const Navbar = () => {
+
+    const { t } = useTranslation()
+    const map = t("map.map")
+
+
     return (<>
         <LanguageSwitch />
         <div>
-            <p>MAP</p>
+            <p>{map}</p>
             {/* will pobably need to make a component just fo the map button so language chages (?)
             !! Need also to ceate the map language inputs in jsons :D */}
         </div>
