e244589a4e495379109b067323f35c1b53bd38e9
Lena Cortes <lena.cortes@usal.es>
2025-09-10 00:00:31 +0200

    changed maps areas bc changign img scale was not compatible

--- src/components/modals/MapModal.jsx
+++ src/components/modals/MapModal.jsx
@@ -1,9 +1,12 @@
 import { useTranslation } from "react-i18next";
 import lowerFloorMap from "../../assets/LowerFloor.png"
 import upperFloorMap from "../../assets/UpperFloor.png"
+import './mapModalCss.css'
+import { useNavigate } from "react-router-dom";
 
 const MapModal = ({ onClose }) => {
     const { t } = useTranslation()
+    const navigate = useNavigate()
 
     const title = t("map.map")
     const lowerFloor = t("map.lowerFloor")
@@ -13,30 +16,52 @@ const MapModal = ({ onClose }) => {
     const projects = t("map.projects")
     const cv = t("map.cv")
 
+    function handleMapClick(place) {
+        navigate(place);
+        onClose()
+    }
+
     return (<div className="mapModalDiv modal-overlay" onClick={onClose}>
         <div className="modal" onClick={(e) => e.stopPropagation()}>
             <button type="button" onClick={onClose}>X</button>
             <h2>{title}</h2>
-            <div className="loweFloor">
-                <h3>{lowerFloor} </h3>
-                <div className="mapContainer">
-                    <img src={lowerFloorMap} alt={lowerFloor} useMap="#lowerFloorMap" className="mapImg" />
-                    <map name="lowerFloorMap">
-                        <area shape="rect" coords="0,0,500,250" href="/frames" alt="frames" />
-                        <area shape="rect" coords="0, 250, 500, 500" href="https://www.linkedin.com/in/lena-webdeveloper/" alt="Diary" />
-                    </map>
+            <div className="floorsMapsContainer">
+                <div className="lowerFloor">
+                    <h3>{lowerFloor} </h3>
+                    <div className="mapContainer">
+                        <img src={lowerFloorMap} alt={lowerFloor} className="mapImg" />
+                        <div
+                            className="clickArea top"
+                            onClick={() => handleMapClick("/frames")}
+                            title={t("map.frames")}
+                        />
+                        <div
+                            className="clickArea bottom"
+                            onClick={() =>
+                                window.open("https://www.linkedin.com/in/lena-webdeveloper/", "_blank")
+                            }
+                            title={t("map.diary")}
+                        />
+                    </div>
+                </div>
+                <div className="upperFloor">
+                    <h3>{upperFloor}</h3>
+                    <div className="mapContainer">
+                        <img src={upperFloorMap} alt={upperFloor} className="mapImg" />
+                        <div
+                            className="clickArea top"
+                            onClick={() => handleMapClick("/cv")}
+                            title={t("map.cv")}
+                        />
+                        <div
+                            className="clickArea bottom"
+                            onClick={() => handleMapClick("/desk")}
+                            title={t("map.projects")}
+                        />
+                    </div>
                 </div>
             </div>
-            <div className="upperFloor">
-                <h3>{upperFloor}</h3>
-                <div className="mapContainer">
-                    <img src={upperFloorMap} alt={upperFloor} useMap="#upperFloorMap" className="mapImg" />
-                    <map name="upperFloorMap">
-                        <area shape="rect" coords="0,0,500,250" href="/cv" alt="CV" />
-                        <area shape="rect" coords="0, 250, 500, 500" href="/desk" alt="desk" />
-                    </map>
-                </div>
-            </div>
+
         </div>
 
     </div>);
